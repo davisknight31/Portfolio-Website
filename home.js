1475d99e121ad62a09e66012e9629e6aba8bd95d
davisknight31 <davisknight7@gmail.com>
2024-05-24 22:56:11 -0400

    add skills section and animations

--- home.js
+++ home.js
@@ -6,3 +6,24 @@ setTimeout(function () {
 setTimeout(function () {
   document.getElementById("fullstack").classList.remove("hidden");
 }, 5000);
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller-inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
